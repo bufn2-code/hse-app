69d95d35c16820aac103f7bc1341cf70e1e77aa4
bufn2-code <pgwawg@gmail.com>
2026-05-26 18:49:14 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+import './index.css' // <-- INI SANGAT PENTING
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
